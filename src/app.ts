1a9f357c6bed7a7add1e19a94a1102ff2c609844
Misael Miranda <misaelbahia1@hotmail.com>
2024-06-17 20:30:16 -0300

    update cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express, { Application } from 'express';
 import { router as useRouter } from './router/receive.router';
+import cors from 'cors';
 
 const app: Application = express();
-
+app.use(cors());
 app.use(express.json());
 //app.use('/send', useRouter);
 app.use('/receive', useRouter);
