5ea600e90e7032e06458c78abee655645b6971e9
Misael Miranda <misaelbahia1@hotmail.com>
2024-06-26 20:36:11 -0300

    https

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
 import app from './app';
+import fs from 'fs';
+import https from 'https';
+
+const privateKey = fs.readFileSync(
+  '/etc/letsencrypt/live/misael.ddns.net/privkey.pem',
+  'utf8'
+);
+const certificate = fs.readFileSync(
+  '/etc/letsencrypt/live/misael.ddns.net/fullchain.pem',
+  'utf8'
+);
+
+const credentials = { key: privateKey, cert: certificate };
 
 const port = 5000;
 
-app.listen(port, (): void => {
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, (): void => {
   console.log(`Server is running at http://localhost:${port}`);
 });
